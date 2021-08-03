266fd5784eb6c16c921197eefacca620a45be859
duha2001 <anhduhuynh2001@gmail.com>
2021-08-03 17:46:55 +0700

    3/8 - Add all file

---
+++ asset/main.js
@@ -0,0 +1,65 @@
+let formBtn=document.querySelector('.fa-user');
+let loginForm=document.querySelector('.login-form-container');
+let formClose=document.querySelector('#form-close');
+let slides=document.querySelectorAll('.slide-container');
+let index=0;
+
+formBtn.addEventListener('click',()=>{
+    loginForm.classList.add('active');
+})
+formClose.addEventListener('click',()=>{
+    loginForm.classList.remove('active')
+;})
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1)%slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index=(index - 1 + slides.length)%slides.length;
+    slides[index].classList.add('active');
+    console.log(index);
+}
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+    },
+});
+
+document.querySelectorAll('.featured-image-1').forEach(image_1 =>{
+    image_1.addEventListener('click',()=>{
+        var src=image_1.getAttribute('src');
+        document.querySelector('.big-image-1').src=src;
+    });
+});
+
+document.querySelectorAll('.featured-image-2').forEach(image_2=>{
+    image_2.addEventListener('click',()=>{
+        var src=image_2.getAttribute('src');
+        document.querySelector('.big-image-2').src=src;
+    });
+});
+
+document.querySelectorAll('.featured-image-3').forEach(image_3=>{
+    image_3.addEventListener('click',()=>{
+        var src =image_3.getAttribute('src');
+        document.querySelector('.big-image-3').src=src;
+    })
+})
